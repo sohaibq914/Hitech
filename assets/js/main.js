8bfffb5e3a6e629d38144c313c2e82fcbe201166
sohaibq914 <sohaibq914@gmail.com>
2023-06-21 17:54:46 -0400

    added mobile tiles content blocks

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,17 @@ window.onload = function () {
     menu_btn.classList.toggle("is-active");
     mobile_menu.classList.toggle("is-active");
   });
+
+  const image = document.querySelector("#changing-image");
+  const images = ["./assets/images/stripper.png", "./assets/images/agglutinationviewer.png", "./assets/images/Blood-Bag-Shaker.avif"];
+  setInterval(function () {
+    const random = Math.floor(Math.random() * 3);
+    image.src = images[random];
+  }, 3000);
+
+  const mobileImage = document.querySelector("#mobile-changing-image");
+  setInterval(function () {
+    const random = Math.floor(Math.random() * 3);
+    mobileImage.src = images[random];
+  }, 3000);
 };
