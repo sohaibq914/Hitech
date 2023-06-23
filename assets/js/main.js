a5cb353eb397d0589606c4745791533d8997bb1e
sohaibq914 <sohaibq914@gmail.com>
2023-06-22 20:10:03 -0400

    attempting to add navbar dropdowns

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,25 @@
 window.onload = function () {
   const menu_btn = document.querySelector(".hamburger");
-  const mobile_menu = document.querySelector(".mobile-nav");
+  const mobile_menu = document.querySelector("nav");
 
   menu_btn.addEventListener("click", function () {
     menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("mobile-nav");
     mobile_menu.classList.toggle("is-active");
   });
+
+  function removeMobileNavClass() {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth > 992) {
+      mobile_menu.classList.remove("mobile-nav");
+      mobile_menu.classList.remove("is-active");
+    }
+  }
+  // Run the function initially when the page loads
+  removeMobileNavClass();
+  // Add an event listener to detect window resize
+  window.addEventListener("resize", removeMobileNavClass);
 
   const image = document.querySelector("#changing-image");
   const images = ["./assets/images/stripper.png", "./assets/images/agglutinationviewer.png", "./assets/images/Blood-Bag-Shaker.avif"];
