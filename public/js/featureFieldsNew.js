bc103acad1a9310dbe403087e18775867bcb260b
sohaibq914 <sohaibq914@gmail.com>
2024-03-14 16:58:07 -0400

    minor fix on feature fields new

--- public/js/featureFieldsNew.js
+++ public/js/featureFieldsNew.js
@@ -1,19 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   const container = document.querySelector(".features-container");
-  const addButtonContainer = container.querySelector(".input-group"); // The container of the "+" button
-
-  const addFeatureBtn = addButtonContainer.querySelector(".add-feature");
+  const addFeatureBtn = container.querySelector(".add-feature");
 
   addFeatureBtn.addEventListener("click", function () {
     const newInput = document.createElement("div");
     newInput.classList.add("input-group", "mb-3");
     newInput.innerHTML = `
-            <input type="text" class="form-control" name="features[]" required>
+            <input type="text" class="form-control" name="features[]">
             <button class="btn btn-danger remove-feature" style="width: 35px;" type="button">-</button>
-            <div class="valid-feedback">Looks good!</div>
         `;
-    // Insert newInput before the addButtonContainer instead of addFeatureBtn
-    container.insertBefore(newInput, addButtonContainer);
+    container.appendChild(newInput);
   });
 
   container.addEventListener("click", function (event) {
