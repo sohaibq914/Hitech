2278ca9bfe88fccbc6ddd346f23ef4be889b670e
sohaibq914 <sohaibq914@gmail.com>
2024-08-08 15:17:01 -0400

    fixed overlay btn

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -24,12 +24,14 @@ paginate.addEventListener("click", function (e) {
 function generateProduct(product) {
   let template = `<div class="col-6 col-lg-3">
   <div class="prod-container">
-    <a href="/products/${product._id}">
+    <a class="plain-link" href="/products/${product._id}">
       <img alt="" class="prod-img img-fluid" src="${product.images.length ? product.images[0].url : "https://st4.depositphotos.com/14953852/24787/v/450/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg"}" >
     </a>
     <p class="price">$${product.price}</p>
     <div class="overlay">
-      <i class="fa-solid fa-cart-shopping d-flex" style="color: white"> <span class="cart-text">Add to cart</span></i>
+      <a class="plain-link" href="/products/${product._id}">
+        <i class="fa-solid fa-cart-shopping d-flex" style="color: white"> <span class="cart-text">View Product</span></i>
+      </a>
     </div>
   </div>
   <h1 class="prod-name">${product.name}</h1>
