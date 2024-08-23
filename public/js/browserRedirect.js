0b10ecd339399f4987e01fb8a445f69aa04ce261
sohaibq914 <sohaibq914@gmail.com>
2024-08-22 21:42:57 -0400

    possible solution for ios insta embed

--- public/js/browserRedirect.js
+++ public/js/browserRedirect.js
@@ -6,8 +6,7 @@ function redirectToExternalBrowser() {
       // Redirect to Chrome on Android
       window.location.href = "intent://www.hitechinstrument.us#Intent;scheme=https;package=com.android.chrome;end";
     } else if (/iPhone|iPad|iPod/i.test(userAgent)) {
-      // Open link in Safari for iOS users
-      window.open("https://www.hitechinstrument.us", "_blank");
+      alert("For a better experience, please open this link in Safari.");
     } else {
       // Default fallback for other devices
       window.location.href = "https://www.hitechinstrument.us";
